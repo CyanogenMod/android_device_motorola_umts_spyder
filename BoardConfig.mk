70af6597f17a3bd001a96dfb30d019cd01fdbe3c
dhacker29 <davidhackerdvm@gmail.com>
2012-08-19 03:43:22 -0500

    udate for kexec

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,16 @@
+# define OMAP_ENHANCEMENT variables
+include device/motorola/spyder/Config.mk
+
 # Camera
 USE_CAMERA_STUB := false
 BOARD_USES_TI_CAMERA_HAL := true
 TI_CAMERAHAL_DEBUG_ENABLED := true
 
-# ICS Kernel Hacks
-BOARD_OVERRIDE_FB0_WIDTH := 540
-BOARD_OVERRIDE_FB0_HEIGHT := 960
+OMAP_ENHANCEMENT_MULTIGPU := true
+
+ENHANCED_DOMX := true
+#USE_ITTIAM_AAC := true
+#BLTSVILLE_ENHANCEMENT :=true
 
 # inherit from the proprietary version
 -include vendor/motorola/umts_spyder/BoardConfigVendor.mk
@@ -22,34 +27,33 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-NEEDS_ARM_ERRATA_754319_754320 := true
-TARGET_GLOBAL_CFLAGS += -DNEEDS_ARM_ERRATA_754319_754320
 
 
 # Kernel
-BOARD_KERNEL_CMDLINE := omap_wdt.timer_margin=60 oops=panic console=/dev/null rw mem=1023M@0x80000000 vram=10300K omapfb.vram=0:8256K,1:4K,2:2040K init=/init ip=off mmcparts=mmcblk1:p7(pds),p8(utags),p14(boot),p15(recovery),p16(cdrom),p17(misc),p18(cid),p19(kpanic),p20(system),p21(cache),p22(preinstall),p23(webtop),p24(userdata),p25(emstorage) mot_sst=1
+BOARD_KERNEL_CMDLINE := root=/dev/ram0 rw mem=1024M@0x80000000 console=ttyO2,115200n8 init=/init ip=off mmcparts=mmcblk1:p7(pds),p8(utags),p14(boot),p15(recovery),p16(cdrom),p17(misc),p18(cid),p19(kpanic),p20(system),p21(cache),p22(preinstall),p23(webtop),p24(userdata),p25(emstorage) mot_sst=1 androidboot.bootloader=0x0A72
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_PAGE_SIZE := 0x4096
 
 # Kernel Build
-TARGET_KERNEL_SOURCE := kernel/motorola/omap4_xt912
-TARGET_KERNEL_CONFIG := mapphone_defconfig
-TARGET_PREBUILT_KERNEL := device/motorola/umts_spyder/kernel
+TARGET_KERNEL_SOURCE := kernel/motorola/solana
+TARGET_KERNEL_CONFIG := hashcode_1024_defconfig
 
 KERNEL_EXTERNAL_MODULES:
-	make -C kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
-	mv kernel/motorola/omap4_xt912/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
-#	make -C kernel/motorola/omap4_xt912/external/vpndriver KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
-#	mv kernel/motorola/omap4_xt912/external/vpndriver/vpnclient.ko $(KERNEL_MODULES_OUT)
-
-#$(KERNEL_OUT)
+	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
+	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+	make clean -C vendor/motorola/common/proprietary/imgtec/eurasia_km/eurasiacon/build/linux2/omap4430_android
+	make -j8 -C vendor/motorola/common/proprietary/imgtec/eurasia_km/eurasiacon/build/linux2/omap4430_android ARCH=arm KERNEL_CROSS_COMPILE=arm-eabi- CROSS_COMPILE=arm-eabi- KERNELSRC=$(KERNEL_OUT)/../../../../../../kernel/motorola/solana KERNELDIR=$(KERNEL_OUT) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+	mkdir $(TARGET_ROOT_OUT)/modules
+	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm_sgx540_120.ko $(KERNEL_MODULES_OUT)
 
 TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
+
 
 # Storage / Sharing
 BOARD_VOLD_MAX_PARTITIONS := 100
@@ -58,18 +62,19 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_stora
 BOARD_MTP_DEVICE := "/dev/mtp"
 
 # Connectivity - Wi-Fi
+USES_TI_MAC80211 := true
+ifdef USES_TI_MAC80211
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+WPA_SUPPLICANT_VERSION           := VER_0_8_X_TI
 BOARD_HOSTAPD_DRIVER             := NL80211
 PRODUCT_WIRELESS_TOOLS           := true
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
 WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
 WIFI_FIRMWARE_LOADER             := ""
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+endif
 
 # Audio
 BOARD_USES_GENERIC_AUDIO := false
@@ -87,7 +92,7 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 
 # Recovery
 BUILD_BOOTMENU_STANDALONE := true
-BOARD_HAS_LOCKED_BOOTLOADER := true
+#BOARD_HAS_LOCKED_BOOTLOADER := true
 TARGET_PREBUILT_RECOVERY_KERNEL := device/motorola/umts_spyder/recovery-kernel
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
@@ -97,7 +102,6 @@ BOARD_MKE2FS := device/motorola/umts_spyder/releaseutils/mke2fs
 BOARD_NONSAFE_SYSTEM_DEVICE := /dev/block/mmcblk1p20
 BOARD_HAS_SDCARD_INTERNAL := true
 #BOARD_HAS_SDEXT := false
-#BOARD_HAS_WEBTOP := true
 TARGET_RECOVERY_PRE_COMMAND := "echo 1 > /data/.recovery_mode; sync;"
 TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
@@ -112,19 +116,23 @@ BOARD_SYSTEM_FILESYSTEM := ext3
 # Graphics
 BOARD_EGL_CFG := device/motorola/umts_spyder/prebuilt/etc/egl.cfg
 USE_OPENGL_RENDERER := true
-COMMON_GLOBAL_CFLAGS += -DSURFACEFLINGER_FORCE_SCREEN_RELEASE
+#COMMON_GLOBAL_CFLAGS += -DSURFACEFLINGER_FORCE_SCREEN_RELEASE
 
 
-# OMAP
-OMAP_ENHANCEMENT := true
 ifdef OMAP_ENHANCEMENT
 COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
-endif
-
-ENHANCED_DOMX := true
-USE_ITTIAM_AAC := true
 ifdef USE_ITTIAM_AAC
 COMMON_GLOBAL_CFLAGS += -DUSE_ITTIAM_AAC
+endif
+ifdef OMAP_ENHANCEMENT_S3D
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_S3D
+endif
+ifdef OMAP_ENHANCEMENT_CPCAM
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_CPCAM
+endif
+ifdef OMAP_ENHANCEMENT_VTC
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT_VTC
+endif
 endif
 
 # MOTOROLA
@@ -147,19 +155,6 @@ TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/motorola/umts_spyder/release
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/motorola/umts_spyder/releasetools/umts_spyder_img_from_target_files
 TARGET_CUSTOM_RELEASETOOL := ./device/motorola/umts_spyder/releasetools/squisher
 
-# CodeAurora Optimizations: msm8960: Improve performance of memmove, bcopy, and memmove_words
-# added by twa_priv
-TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
-TARGET_USE_KRAIT_PLD_SET := true
-TARGET_KRAIT_BIONIC_PLDOFFS := 10
-TARGET_KRAIT_BIONIC_PLDTHRESH := 10
-TARGET_KRAIT_BIONIC_BBTHRESH := 64
-TARGET_KRAIT_BIONIC_PLDSIZE := 64
-
-# Bootanimation
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
-
 
 # Misc.
 BOARD_USE_BATTERY_CHARGE_COUNTER := true
@@ -168,3 +163,6 @@ BOARD_NEEDS_CUTILS_LOG := true
 BOARD_USES_SECURE_SERVICES := true
 BOARD_HAS_MAPPHONE_SWITCH := true
 USE_IPV6_ROUTE := true
+
+# Common device independent definitions
+include device/ti/common-open/BoardConfig.mk
--- device.mk
+++ device.mk
@@ -21,18 +21,15 @@ PRODUCT_PACKAGES := \
 # Audio
 PRODUCT_COPY_FILES += \
     device/motorola/umts_spyder/audio/alsa.omap4.so:/system/lib/hw/alsa.omap4.so \
-    device/motorola/umts_spyder/audio/audio.primary.omap4.so:/system/lib/hw/audio.primary.omap4.so \
-    device/motorola/umts_spyder/audio/audio_policy.omap4.so:/system/lib/hw/audio_policy.omap4.so \
+    device/motorola/umts_spyder/audio/audio.primary.omap4.so:/system/lib/hw/audio.primary.umts_spyder.so \
+    device/motorola/umts_spyder/audio/audio_policy.omap4.so:/system/lib/hw/audio_policy.umts_spyder.so \
     device/motorola/umts_spyder/audio/libasound.so:/system/lib/libasound.so \
     device/motorola/umts_spyder/audio/libaudio_ext.so:/system/lib/libaudio_ext.so
 
-#    device/motorola/umts_spyder/audio/audio.a2dp.default.so:/system/lib/hw/audio.a2dp.default.so \
+# Hardware HALs
 PRODUCT_PACKAGES += \
-    parse_hdmi_edid \
-    libedid \
-    hwcomposer.umts_spyder \
+    camera.omap4 \
     libinvensense_mpl \
-    camera.omap4
 
 PRODUCT_PACKAGES += \
     libaudioutils \
@@ -47,17 +44,12 @@ PRODUCT_PACKAGES += \
     hciconfig \
     hcitool
 
-# Tiler
-PRODUCT_PACKAGES += \
-    libtimemmgr
-
 # Modem
 PRODUCT_PACKAGES += \
     Stk \
     libaudiomodemgeneric \
     rild \
-    radiooptions \
-    sh 
+    radiooptions
 
 # Wifi
 PRODUCT_PACKAGES += \
@@ -70,6 +62,10 @@ PRODUCT_PACKAGES += \
     regulatory.bin \
     calibrator \
     busybox \
+
+# Wifi Direct
+PRODUCT_PACKAGES += \
+    ti_wfd_libs
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -84,13 +80,14 @@ PRODUCT_PACKAGES += \
     umts_spyder_releaseutils-tune2fs
 
 PRODUCT_PACKAGES += \
+    evtest \
     camera_test \
     Camera \
     CameraOMAP4 \
     Superuser \
     su \
     DockAudio \
-
+    parse_hdmi_edid \
 
 PRODUCT_PACKAGES += \
     librs_jni \
@@ -118,76 +115,58 @@ PRODUCT_COPY_FILES += \
 
 # Rootfs files
 PRODUCT_COPY_FILES += \
-    out/target/product/umts_spyder/root/init:system/etc/rootfs/init \
-    out/target/product/umts_spyder/root/sbin/adbd:system/etc/rootfs/sbin/adbd \
-    device/motorola/umts_spyder/root/default.prop:system/etc/rootfs/default.prop \
-    device/motorola/umts_spyder/root/init.rc:system/etc/rootfs/init.rc \
-    device/motorola/umts_spyder/root/init.trace.rc:system/etc/rootfs/init.trace.rc \
-    device/motorola/umts_spyder/root/init.usb.rc:system/etc/rootfs/init.usb.rc \
-    device/motorola/umts_spyder/root/init.mapphone_cdma.rc:system/etc/rootfs/init.mapphone_cdma.rc \
-    device/motorola/umts_spyder/root/init.mapphone_umts.rc:system/etc/rootfs/init.mapphone_umts.rc \
-    device/motorola/umts_spyder/root/ueventd.rc:system/etc/rootfs/ueventd.rc \
-    device/motorola/umts_spyder/root/ueventd.mapphone_cdma.rc:system/etc/rootfs/ueventd.mapphone_cdma.rc \
-    device/motorola/umts_spyder/root/ueventd.mapphone_umts.rc:system/etc/rootfs/ueventd.mapphone_umts.rc \
+    device/motorola/umts_spyder/root/default.prop:/root/default.prop \
+    device/motorola/umts_spyder/root/init.rc:/root/init.rc \
+    device/motorola/umts_spyder/root/init.mapphone_cdma.rc:/root/init.mapphone_cdma.rc \
+    device/motorola/umts_spyder/root/init.mapphone_umts.rc:/root/init.mapphone_umts.rc \
+    device/motorola/umts_spyder/root/ueventd.rc:/root/ueventd.rc \
+    device/motorola/umts_spyder/root/ueventd.mapphone_cdma.rc:/root/ueventd.mapphone_cdma.rc \
+    device/motorola/umts_spyder/root/ueventd.mapphone_umts.rc:/root/ueventd.mapphone_umts.rc \
 
-# Hijack files
+# Kexec files
 PRODUCT_COPY_FILES += \
-    device/motorola/umts_spyder/root/default.prop:root/default.prop \
-    device/motorola/umts_spyder/root/init.rc:root/init.rc \
-    device/motorola/umts_spyder/root/init.trace.rc:root/init.trace.rc \
-    device/motorola/umts_spyder/root/init.usb.rc:root/init.usb.rc \
-    device/motorola/umts_spyder/root-hijack/init.mapphone_cdma.rc:root/init.mapphone_cdma.rc \
-    device/motorola/umts_spyder/root-hijack/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
-    device/motorola/umts_spyder/root/ueventd.rc:root/ueventd.rc \
-    device/motorola/umts_spyder/root/ueventd.mapphone_cdma.rc:root/ueventd.mapphone_cdma.rc \
-    device/motorola/umts_spyder/root/ueventd.mapphone_umts.rc:root/ueventd.mapphone_umts.rc \
-
+    device/motorola/umts_spyder/kexec/arm_kexec.ko:system/etc/kexec/arm_kexec.ko \
+    device/motorola/umts_spyder/kexec/atags:system/etc/kexec/atags \
+    device/motorola/umts_spyder/kexec/devtree:system/etc/kexec/devtree \
+    device/motorola/umts_spyder/kexec/kexec:system/etc/kexec/kexec \
+    device/motorola/umts_spyder/kexec/kexec.ko:system/etc/kexec/kexec.ko \
+    device/motorola/umts_spyder/kexec/uart.ko:system/etc/kexec/uart.ko \
+    out/target/product/umts_spyder/ramdisk.img:system/etc/kexec/ramdisk.img \
+    out/target/product/umts_spyder/kernel:system/etc/kexec/kernel \
 
 # Permissions files
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:/system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:/system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:/system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:/system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:/system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:/system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:/system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:/system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:/system/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:/system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:/system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:/system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
 
-#    frameworks/native/data/etc/android.hardware.wifi.direct.xml:/system/etc/permissions/android.hardware.wifi.direct.xml \
 # Prebuilts
 PRODUCT_COPY_FILES += \
     device/motorola/umts_spyder/prebuilt/bin/battd:system/bin/battd \
-    device/motorola/umts_spyder/prebuilt/bin/hijack:system/bin/hijack \
-    device/motorola/umts_spyder/prebuilt/bin/hijack.log_dump:system/bin/hijack.log_dump \
     device/motorola/umts_spyder/prebuilt/bin/mount_ext3.sh:system/bin/mount_ext3.sh \
     device/motorola/umts_spyder/prebuilt/bin/strace:system/bin/strace \
     device/motorola/umts_spyder/prebuilt/etc/gps.conf:system/etc/gps.conf \
     device/motorola/umts_spyder/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
-    device/motorola/umts_spyder/prebuilt/etc/hijack-boot.zip:system/etc/hijack-boot.zip \
     device/motorola/umts_spyder/prebuilt/etc/TICameraCameraProperties.xml:system/etc/TICameraCameraProperties.xml \
     device/motorola/umts_spyder/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
     device/motorola/umts_spyder/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/motorola/spyder/prebuilt/etc/firmware/ducati-m3.bin:system/etc/firmware/ducati-m3.bin \
 
 #    device/motorola/umts_spyder/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
-# omapzoom wifi firmware
-PRODUCT_COPY_FILES += \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin:/system/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
-    device/motorola/umts_spyder/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin:/system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
@@ -195,10 +174,6 @@ PRODUCT_COPY_FILES += \
     device/motorola/umts_spyder/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
     device/motorola/umts_spyder/prebuilt/bin/50-backup.sh:system/addon.d/50-backup.sh
 
-# prebuilt hwc
-PRODUCT_COPY_FILES += \
-    device/motorola/umts_spyder/hwc/hwcomposer.spyder.so:system/vendor/lib/hw/hwcomposer.spyder.so \
-    device/motorola/umts_spyder/hwc/libedid.so:system/lib/libedid.so
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -207,27 +182,13 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_LOCALES += en_US
 
 
-# copy all kernel modules under the "modules" directory to system/lib/modules
-PRODUCT_COPY_FILES += $(shell \
-    find device/motorola/umts_spyder/modules -name '*.ko' \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/motorola/umts_spyder/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
 # stuff specific to ti OMAP4 hardware
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
-
+$(call inherit-product, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
+$(call inherit-product-if-exists, vendor/motorola/common/proprietary/imgtec/sgx-imgtec-bins.mk)
 $(call inherit-product-if-exists, vendor/motorola/umts_spyder/umts_spyder-vendor.mk)
 
-
-# stuff common to all Motorola phones -- disabled for Sandbox
-#$(call inherit-product, device/motorola/common/common_hijack.mk)
