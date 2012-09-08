7d6da834af3ed02bb317664560f310409c4d4dbd
David Hacker <davidhackerdvm@gmail.com>
2012-09-07 21:12:50 -0500

    CM10 board cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_umts_spyder.mk \
-    $(LOCAL_DIR)/aokp.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_umts_spyder.mk
+ifeq ($(TARGET_PRODUCT),aokp_umts_spyder)
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
+endif
