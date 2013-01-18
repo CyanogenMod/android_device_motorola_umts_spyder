684e763129de20a2603f623bda2d2574fd2b87e5
dhacker29 <davidhackerdvm@gmail.com>
2013-01-17 23:49:16 -0600

    Build audio_policy from source

--- device.mk
+++ device.mk
@@ -22,10 +22,10 @@ endif
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/audio/alsa.omap4.so:/system/lib/hw/alsa.omap4.so \
     $(DEVICE_FOLDER)/audio/audio.primary.omap4.so:/system/lib/hw/audio.primary.umts_spyder.so \
-    $(DEVICE_FOLDER)/audio/audio_policy.omap4.so:/system/lib/hw/audio_policy.omap4.so \
     $(DEVICE_FOLDER)/audio/libaudio_ext.so:/system/lib/libaudio_ext.so
 
 PRODUCT_PACKAGES += \
+    audio_policy.umts_spyder \
     libasound
 
 # Modem
