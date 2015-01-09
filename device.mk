338d511995e5908de92d85c200ffd090f7d41c91
Michael Gernoth <michael@gernoth.net>
2015-01-09 14:43:31 +0100

    umts_spyder: use "safestrapped" ramdisk
    Change-Id: I61142dcf6b0e294af2e0c3c88c0137a31811c9d0

--- device.mk
+++ device.mk
@@ -35,10 +35,10 @@ PRODUCT_PACKAGES += \
     Stk
 
 # Kexec files
+PRODUCT_PACKAGES += etc/kexec/ramdisk.img
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/init.target.rc:root/init.target.rc \
     $(DEVICE_FOLDER)/kexec/devtree:system/etc/kexec/devtree \
-    $(OUT)/ramdisk.img:system/etc/kexec/ramdisk.img \
     $(OUT)/kernel:system/etc/kexec/kernel
 
 # Prebuilts
